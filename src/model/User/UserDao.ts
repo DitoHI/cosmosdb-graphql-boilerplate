62d2bf0a243b6cb1472fc09b06c00e9a5482c808
ditohafizh <ditohafizh@gmail.com>
2019-03-17 13:52:58 +0700

    fix indentation in resolver, controller, and dao

--- src/model/User/UserDao.ts
+++ src/model/User/UserDao.ts
@@ -8,51 +8,69 @@ class UserDao extends Dao {
 
   async addUser(user: any) {
     user.isActived = true;
-    return this.container.items.create(user).then((result) => {
-      return result.body;
-    }).catch((err) => {
-      return err;
-    });
+    return this.container.items
+      .create(user)
+      .then((result) => {
+        return result.body;
+      })
+      .catch((err) => {
+        return err;
+      });
   }
 
   async updateUser(userId: string, user: any) {
-    return this.getItem(userId).then((doc) => {
-      if (doc == null) {
-        return new Error('No user found');
-      }
-
-      for (const key in user) {
-        if (user.hasOwnProperty(key)) {
-          doc[key] = user[key];
+    return this
+      .getItem(userId)
+      .then((doc) => {
+        if (doc == null) {
+          return new Error('No user found');
         }
-      }
 
-      return this.container.item(userId).replace(doc).then((result) => {
-        return result.body;
-      }).catch((err) => {
-        return err;
+        for (const key in user) {
+          if (user.hasOwnProperty(key)) {
+            doc[key] = user[key];
+          }
+        }
+
+        return this.container
+          .item(userId)
+          .replace(doc)
+          .then((result) => {
+            return result.body;
+          })
+          .catch((err) => {
+            return err;
+          });
       });
-    });
   }
 
   async getItem(userId: string) {
-    return this.container.item(userId).read().then((result) => {
-      return result.body;
-    }).catch((err) => {
-      return err;
-    });
+    return this.container
+      .item(userId)
+      .read()
+      .then((result) => {
+        return result.body;
+      })
+      .catch((err) => {
+        return err;
+      });
   }
 
   async deleteUser(userId: string) {
-    return this.getItem(userId).then((result) => {
-      if (result == null) {
-        return new Error('No user found');
-      }
+    return this
+      .getItem(userId)
+      .then((result) => {
+        if (result == null) {
+          return new Error('No user found');
+        }
 
-      this.container.item(userId).delete().catch((err) => {
-        return new Error(err);
+        this.container
+          .item(userId)
+          .delete()
+          .catch((err) => {
+            return new Error(err);
+          });
       });
-    });
   }
 }
 
--- src/resolver/User/Users.ts
+++ src/resolver/User/Users.ts
@@ -70,73 +70,98 @@ export const resolvers: IResolvers = {
   URL,
   Mutation: {
     addUser: (async (_, user, { userController }) => {
-      return userController.addUser(user).then((result: any) => {
-        return result;
-      }).catch((err: Error) => {
-        throw err;
-      });
+      return userController
+        .addUser(user)
+        .then((result: any) => {
+          return result;
+        })
+        .catch((err: Error) => {
+          throw err;
+        });
     }),
     deleteUser: (async (_, user, { userController }) => {
-      return userController.deleteUser(user).then((result: any) => {
-        return result;
-      }).catch((err: any) => {
-        throw err;
-      });
+      return userController
+        .deleteUser(user)
+        .then((result: any) => {
+          return result;
+        })
+        .catch((err: any) => {
+          throw err;
+        });
     }),
     updateStatus: (async (_, user , { userController }) => {
-      return userController.updateUser(user, user.updatedIsActived).then((result: any) => {
-        return result;
-      }).catch((err: Error) => {
-        throw err;
-      });
+      return userController
+        .updateUser(user, user.updatedIsActived)
+        .then((result: any) => {
+          return result;
+        })
+        .catch((err: Error) => {
+          throw err;
+        });
     }),
     updateUser: (async (_, user, { userController }) => {
       user.isActived = true;
-      return userController.updateUser(user).then((result: any) => {
-        return result;
-      }).catch((err: Error) => {
-        throw err;
-      });
+      return userController
+        .updateUser(user)
+        .then((result: any) => {
+          return result;
+        })
+        .catch((err: Error) => {
+          throw err;
+        });
     }),
     updateEducation: (async (_, education, { userController }) => {
-      return userController.updateEducation(education).then((result: any) => {
-        return result;
-      }).catch((err: Error) => {
-        throw err;
-      });
+      return userController
+        .updateEducation(education)
+        .then((result: any) => {
+          return result;
+        })
+        .catch((err: Error) => {
+          throw err;
+        });
     }),
     updateExperience: (async (_, experience, { userController }) => {
-      return userController.updateExperience(experience).then((result: any) => {
-        return result;
-      }).catch((err: Error) => {
-        throw err;
-      });
+      return userController
+        .updateExperience(experience)
+        .then((result: any) => {
+          return result;
+        })
+        .catch((err: Error) => {
+          throw err;
+        });
     }),
     updateProject: (async (_, project, { userController }) => {
-      return userController.updateProject(project).then((result: any) => {
-        return result;
-      }).catch((err: Error) => {
-        throw err;
-      });
+      return userController
+        .updateProject(project)
+        .then((result: any) => {
+          return result;
+        })
+        .catch((err: Error) => {
+          throw err;
+        });
     }),
   },
   Query: {
     me: (async (_, {}, { userController }) => {
-      return userController.showUsers({ isActived: true }).then((result: any) => {
-        if (result.length > 1) {
-          throw new Error(`There are ${result.length} users found who are actived. ` +
+      return userController
+        .showUsers({ isActived: true })
+        .then((result: any) => {
+          if (result.length > 1) {
+            throw new Error(`There are ${result.length} users found who are actived. ` +
             'Please inactive the others');
-        }
-        return result[0];
-      });
+          }
+          return result[0];
+        });
     }),
     users: (async (_, user, { userController }) => {
-      return userController.showUsers(user).then((result: any) => {
-        if (result.length === 0) {
-          throw new Error('User not found');
-        }
-        return result;
-      });
+      return userController
+        .showUsers(user)
+        .then((result: any) => {
+          if (result.length === 0) {
+            throw new Error('User not found');
+          }
+          return result;
+        });
     }),
   },
 };
